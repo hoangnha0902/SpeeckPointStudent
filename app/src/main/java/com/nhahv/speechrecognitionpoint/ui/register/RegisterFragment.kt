6f28c31e4c86277649d5cc8f3a36b6687767a4f7
hoang.van.nha <hoang.van.nha@framgia.com>
2018-10-22 15:10:19 +0700

    Task 13: logic login

--- app/src/main/java/com/nhahv/speechrecognitionpoint/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/nhahv/speechrecognitionpoint/ui/register/RegisterFragment.kt
@@ -3,18 +3,21 @@ package com.nhahv.speechrecognitionpoint.ui.register
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.text.TextUtils
+import android.text.method.PasswordTransformationMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.navigation.NavOptions
+import androidx.navigation.findNavController
 import com.nhahv.speechrecognitionpoint.R
+import com.nhahv.speechrecognitionpoint.util.Constant.PASSWORD
+import com.nhahv.speechrecognitionpoint.util.Constant.USER_NAME
+import com.nhahv.speechrecognitionpoint.util.SharedPrefs
+import com.nhahv.speechrecognitionpoint.util.toast
+import kotlinx.android.synthetic.main.register_fragment.*
 
 class RegisterFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = RegisterFragment()
-    }
-
     private lateinit var viewModel: RegisterViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -26,6 +29,37 @@ class RegisterFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(RegisterViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
+
+    private fun initViews() {
+        register.setOnClickListener {
+            if (TextUtils.isEmpty(userName.text.toString())) {
+                toast("Nhập tên tài khoản")
+                return@setOnClickListener
+            }
+            if (TextUtils.isEmpty(password.text.toString()) || TextUtils.isEmpty(rePassword.text.toString()) || password.text.toString() != rePassword.text.toString()) {
+                toast("Nhập đúng mật khẩu")
+                return@setOnClickListener
+            }
+            SharedPrefs.getInstance(requireContext()).put(USER_NAME, userName.text.toString())
+            SharedPrefs.getInstance(requireContext()).put(PASSWORD, password.text.toString())
+            register.findNavController().navigate(R.id.action_register_to_class, null, NavOptions.Builder().setClearTask(true).build())
+
+        }
+        showPassword.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                password.transformationMethod = null
+                rePassword.transformationMethod = null
+            } else {
+                password.transformationMethod = PasswordTransformationMethod()
+                rePassword.transformationMethod = PasswordTransformationMethod()
+            }
+        }
     }
 
 }
